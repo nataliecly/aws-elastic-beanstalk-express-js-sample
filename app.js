fd536443a063431dd2dd1d4d948694d6e8977f7d
Natalie Yu <nyu17@my.bcit.ca>
2024-11-04 10:55:19 -0800

    change message again

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('COMP4964 Lab 7'));
+app.get('/', (req, res) => res.send('Hello again!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
